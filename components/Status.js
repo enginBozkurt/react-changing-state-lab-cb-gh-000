9559ba7a7582dfd2efec9aec2fa5176220af27b1
Engin Bozkurt <engin.bozkurt034@gmail.com>
2018-07-30 14:09:46 +0000

    automatically backed up by learn

--- components/Status.js
+++ components/Status.js
@@ -3,12 +3,9 @@ import React from 'react';
 export default class Status extends React.Component {
   render () {
     const { winner } = this.props;
-    let status = winner;
-    (winner !== 'X' && winner !== 'O') ? status = 'Tie' : status += ' wins';
-
     return (
-      <div className="status">
-        {status}
+      <div className='status'>
+        {winner ? `${winner} wins` : "Tie"}
       </div>
     );
   }
